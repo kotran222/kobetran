708dff7034ac408d48006f2ba9fc4874a9569cf6
kotran222 <kobetrankhoi222@gmail.com>
2020-11-04 23:12:37 -0800

    bug fixes and updates

---
+++ public/main.js
@@ -0,0 +1,18 @@
+// Main JS for kobetran website
+
+// SIDE MENU
+// Side Menu Functions
+function openSlideMenu(){
+    document.getElementById('side-menu').style.width = '250px';
+    document.getElementById('main').style.marginRight = '250px';
+}
+
+function closeSlideMenu(){
+    document.getElementById('side-menu').style.width = '0';
+    document.getElementById('main').style.marginRight = '0';
+}
+
+
+
+
+
